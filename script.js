f543844168ac74a7b208cca0a664d11580a1f3d4
Alka <sfnalkatiri@gmail.com>
2021-01-12 18:41:01 +0700

    prototype

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 //function declaration
-const methodmahasiswa = {
-	makan: function (porsi) {
-		this.energi += porsi;
-		console.log(`Halo ${this.name} energi ${this.energi}`);
-	},
-	tidur: function (jam) {
-		this.energi += jam * 2;
-	},
-};
+// const methodmahasiswa = {
+// 	makan: function (porsi) {
+// 		this.energi += porsi;
+// 		console.log(`Halo ${this.name} energi ${this.energi}`);
+// 	},
+// 	tidur: function (jam) {
+// 		this.energi += jam * 2;
+// 	},
+// };
 
+// function Mahasiswa(nama, energi) {
+// 	let mahasiswa = Object.create(methodmahasiswa);
+// 	mahasiswa.nama = nama;
+// 	mahasiswa.energi = energi;
+
+// 	return mahasiswa;
+// }
+
+//PROTOTYPE
 function Mahasiswa(nama, energi) {
-	let mahasiswa = Object.create(methodmahasiswa);
-	mahasiswa.nama = nama;
-	mahasiswa.energi = energi;
-
-	return mahasiswa;
+	this.nama = nama;
+	this.energi = energi;
 }
 
-let alka = Mahasiswa("alka", 23);
+Mahasiswa.prototype.makan = function (porsi) {
+	this.energi += porsi;
+	console.log(`Halo ${this.nama} energi ${this.energi}`);
+};
+
+let alka = new Mahasiswa("alka", 23);
