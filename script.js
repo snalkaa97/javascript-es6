831eaf3cd784114a5992eadc0db775710e95288d
Alka <sfnalkatiri@gmail.com>
2021-01-12 18:12:22 +0700

    object declaration with literal

--- script.js
+++ script.js
@@ -1,31 +1,22 @@
 //function declaration
+const methodmahasiswa = {
+	makan: function (porsi) {
+		this.energi += porsi;
+		console.log(`Halo ${this.name} energi ${this.energi}`);
+	},
+	tidur: function (jam) {
+		this.energi += jam * 2;
+	},
+};
+
 function Mahasiswa(nama, energi) {
 	let mahasiswa = {};
 	mahasiswa.nama = nama;
 	mahasiswa.energi = energi;
-
-	mahasiswa.makan = function (porsi) {
-		this.energi += porsi;
-		console.log(`Halo ${this.name} energi ${this.energi}`);
-	};
+	mahasiswa.makan = methodmahasiswa.makan;
+	mahasiswa.tidur = methodmahasiswa.tidur;
 
 	return mahasiswa;
 }
 
 let alka = Mahasiswa("alka", 23);
-
-//function constructor
-function Mahasiswa(nama, energi) {
-	// let mahasiswa = {};
-	this.nama = nama;
-	this.energi = energi;
-
-	this.makan = function (porsi) {
-		this.energi += porsi;
-		console.log(`Halo ${this.name} energi ${this.energi}`);
-	};
-}
-
-let alkatiri = new Mahasiswa("alkatiri", 23);
-
-//Object Create
